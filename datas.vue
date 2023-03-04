e902ceb5563609b7279216deaf9457fc2d4a0087
tweedlexcd <126968542+tweedlexcd@users.noreply.github.com>
2023-03-04 16:59:16 +0200

    Create datas.vue

---
+++ datas.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="fitlab_goods">
+    <div class="card__product-container">
+      <div class="trainers__title">
+        {{ staticText.product.otherModel[actualLang.index] }}
+        
+            </div>
+      </section>
+</template>
+        
+<script>
+  import { mapState } from 'vuex';
+  import { staticText } from '../i18n';
+</script>
+
+  computed: {
+    ...mapState({
+      product: (state) => state.products.product,
+      actualLang: (state) => state.settings.actualLang,
+    }),

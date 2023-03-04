7520d71ac5e7d3839e63283c96872c345a0ef1c6
tweedlexcd <126968542+tweedlexcd@users.noreply.github.com>
2023-03-04 17:02:10 +0200

    Create data from exersises.vue

---
+++ data/data from exersises.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="online courses">
+      </div>
+      
+            <tr>
+              <td>
+                <ul>
+                  <li>cardio</li>
+                </ul>
+              </td>
+              <td>
